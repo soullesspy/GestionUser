0dfcb3442b2aa8e70ffcd6cf5713797a91deb659
Atlom <30836095+atlom@users.noreply.github.com>
2017-08-19 22:58:45 -0400

    Tarea1

---
+++ build/generated-sources/ap-source-output/com/gu/entidades/Usuarios_.java
@@ -0,0 +1,15 @@
+package com.gu.entidades;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-08-19T22:15:00")
+@StaticMetamodel(Usuarios.class)
+public class Usuarios_ { 
+
+    public static volatile SingularAttribute<Usuarios, String> correo;
+    public static volatile SingularAttribute<Usuarios, Integer> id;
+    public static volatile SingularAttribute<Usuarios, String> nombre;
+
+}
